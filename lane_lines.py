b864571b99269e90b6c0b7c5c58ab0a56822190e
fvmassoli <fabiovaleriomassoli@gmail.com>
2017-04-26 10:45:14 +0200

    Final code implementation

--- lane_lines.py
+++ lane_lines.py
@@ -4,7 +4,7 @@ import cv2
 
 
 class LaneLines():
-    def __init__(self, nwindows=9, margin=100, minpix=50, max_nb_acquired_lanes=15):
+    def __init__(self, nwindows=9, margin=100, minpix=50, max_nb_acquired_lanes=25):
         self.nwindows = nwindows
         self.margin = margin
         self.minpix = minpix
@@ -21,21 +21,20 @@ class LaneLines():
 
     def search(self, binary_warped):
         self.current_image = binary_warped
-        self.set_search_mode(binary_warped)
+        self._set_search_mode(binary_warped)
         if len(self.old_lanes) >= self.max_nb_acquired_lanes:
             self.old_lanes.pop(0)
-        # self.old_lanes.append(copy.copy(self.lane))
         self.old_lanes.append(self)
-        self.smooth_fit(binary_warped)
+        self._average_fit(binary_warped)
 
-    def set_search_mode(self, binary_warp):
+    def _set_search_mode(self, binary_warp):
         '''
         Start a full search for lane lines or uses previously knowledge 
         '''
         if self.full_search_mode:
             self.start_full_search(binary_warp)
         else:
-            self.fast_search(binary_warp)
+            self._start_fast_search(binary_warp)
 
     def start_full_search(self, binary_warped):
         self.left_fit = []
@@ -69,18 +68,11 @@ class LaneLines():
             win_xright_low = rightx_current - self.margin
             win_xright_high = rightx_current + self.margin
 
-            good_left_inds = ((self.nonzeroy >= win_y_low) &
-                              (self.nonzeroy < win_y_high) &
-                              (self.nonzerox >= win_xleft_low) &
-                              (self.nonzerox < win_xleft_high)).nonzero()[0]
+            good_left_inds = ((self.nonzeroy >= win_y_low) & (self.nonzeroy < win_y_high) &
+                              (self.nonzerox >= win_xleft_low) & (self.nonzerox < win_xleft_high)).nonzero()[0]
+            good_right_inds = ((self.nonzeroy >= win_y_low) & (self.nonzeroy < win_y_high) &
+                               (self.nonzerox >= win_xright_low) & (self.nonzerox < win_xright_high)).nonzero()[0]
 
-            good_right_inds = ((self.nonzeroy >= win_y_low) &
-                               (self.nonzeroy < win_y_high) &
-                               (self.nonzerox >= win_xright_low) &
-                               (self.nonzerox < win_xright_high)).nonzero()[0]
-
-            # self.left_lane_inds = np.concatenate(good_left_inds)
-            # self.right_lane_inds = np.concatenate(good_right_inds)
             left_lane_inds.append(good_left_inds)
             right_lane_inds.append(good_right_inds)
 
@@ -98,14 +90,12 @@ class LaneLines():
         self.rightx = self.nonzerox[self.right_lane_inds]
         self.righty = self.nonzeroy[self.right_lane_inds]
 
-        if len(self.leftx) < 25 or len(self.rightx) < 25:
-            # self = self.old
+        if len(self.leftx) < 50 or len(self.rightx) < 50:
             self.leftx = self.old_leftx
             self.lefty = self.old_lefty
             self.rightx = self.old_rightx
             self.righty = self.old_righty
 
-        # Fit a second order polynomial to each
         self.left_fit = np.polyfit(self.lefty, self.leftx, 2)
         self.right_fit = np.polyfit(self.righty, self.rightx, 2)
         self.fity = np.linspace(0, self.binary_warped.shape[0] - 1, self.binary_warped.shape[0])
@@ -117,26 +107,16 @@ class LaneLines():
         self.old_righty = self.righty
         self.exhaustive = False
 
-    def fast_search(self, binary_warped):
+    def _start_fast_search(self, binary_warped):
         self.binary_warped = binary_warped
         self.nonzero = binary_warped.nonzero()
         self.nonzeroy = np.array(self.nonzero[0])
         self.nonzerox = np.array(self.nonzero[1])
-        self.left_lane_inds = ((self.nonzerox > (self.left_fit[0] *
-                                                 (self.nonzeroy ** 2) + self.left_fit[1] * self.nonzeroy +
-                                                 self.left_fit[2] - self.margin)) &
-                               (self.nonzerox < (self.left_fit[0] * (self.nonzeroy ** 2) +
-                                                 self.left_fit[1] * self.nonzeroy +
-                                                 self.left_fit[2] + self.margin)))
 
-        self.right_lane_inds = ((self.nonzerox > (self.right_fit[0] *
-                                                  (self.nonzeroy ** 2) + self.right_fit[1] * self.nonzeroy +
-                                                  self.right_fit[2] - self.margin)) & (self.nonzerox <
-                                                                                       (self.right_fit[0] * (
-                                                                                       self.nonzeroy ** 2) +
-                                                                                        self.right_fit[1] *
-                                                                                        self.nonzeroy + self.right_fit[
-                                                                                            2] + self.margin)))
+        self.left_lane_inds = ((self.nonzerox > (self.left_fit[0] * (self.nonzeroy ** 2) + self.left_fit[1] * self.nonzeroy + self.left_fit[2] - self.margin)) &
+                               (self.nonzerox < (self.left_fit[0] * (self.nonzeroy ** 2) +self.left_fit[1] * self.nonzeroy + self.left_fit[2] + self.margin)))
+        self.right_lane_inds = ((self.nonzerox > (self.right_fit[0] * (self.nonzeroy ** 2) + self.right_fit[1] * self.nonzeroy + self.right_fit[2] - self.margin)) &
+                                (self.nonzerox < (self.right_fit[0] * ( self.nonzeroy ** 2) + self.right_fit[1] * self.nonzeroy + self.right_fit[2] + self.margin)))
 
         self.leftx = self.nonzerox[self.left_lane_inds]
         self.lefty = self.nonzeroy[self.left_lane_inds]
@@ -153,7 +133,7 @@ class LaneLines():
         self.fit_leftx = self.left_fit[0] * self.fity ** 2 + self.left_fit[1] * self.fity + self.left_fit[2]
         self.fit_rightx = self.right_fit[0] * self.fity ** 2 + self.right_fit[1] * self.fity + self.right_fit[2]
 
-    def plot(self):
+    def get_pipeline_result(self):
         out_img = np.dstack(
             (self.binary_warped, self.binary_warped, self.binary_warped)) * 255
         window_img = np.zeros_like(out_img)
@@ -177,17 +157,32 @@ class LaneLines():
         result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
         return result, self.fit_leftx, self.fit_rightx, self.fity
 
-    #         plt.imshow(result)
-    #         plt.plot(self.fit_leftx, self.fity, color='yellow', linewidth=5.0)
-    #         plt.plot(self.fit_rightx, self.fity, color='yellow', linewidth=5.0)
-    #         plt.xlim(0, 1280)
-    #         plt.ylim(720, 0)
+    def create_real_world_image(self, image, Minv):
+        warped = self.binary_warped
+        warp_zero = np.zeros_like(warped).astype(np.uint8)
+        color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
 
-    def smooth_fit(self, binary_warped):
+        pts_left = np.array([np.transpose(np.vstack([self.fit_leftx, self.fity]))])
+        pts_right = np.array([np.flipud(np.transpose(np.vstack([self.fit_rightx, self.fity])))])
+
+        pts = np.hstack((pts_left, pts_right))
+        cv2.fillPoly(color_warp, np.int_([pts]), (0, 255, 0))
+
+        newwarp = cv2.warpPerspective(color_warp, Minv, self.img_shape)
+        left_curvature, right_curvature = self.evaluate_curvature_radius()
+
+        cv2.putText(image, "Curvature: " + str(int(left_curvature)) + ' (m)',
+                    (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), thickness=3)
+        cv2.putText(image, str("%.2f" % self.car_center_offset()),
+                    (100, 170), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), thickness=3)
+
+        result = cv2.addWeighted(image, 1, newwarp, 0.3, 0)
+        return result
+
+    def _average_fit(self, binary_warped):
         self.current_image = binary_warped
         leftx, lefty = [], []
         rightx, righty = [], []
-
         for i, lane in enumerate(self.old_lanes):
             leftx.extend(lane.leftx)
             lefty.extend(lane.lefty)
@@ -203,80 +198,34 @@ class LaneLines():
         self.left_fit = np.polyfit(lefty, leftx, 2)
         self.right_fit = np.polyfit(righty, rightx, 2)
         self.fit_leftx = np.array(self.left_fit[0] * self.fity ** 2 + self.left_fit[1] * self.fity + self.left_fit[2])
-        self.fit_rightx = np.array(
-            self.right_fit[0] * self.fity ** 2 + self.right_fit[1] * self.fity + self.right_fit[2])
-        self._set_variables(leftx, rightx, lefty, righty, self.left_fit, self.right_fit, self.fit_leftx,
-                            self.fit_rightx)
+        self.fit_rightx = np.array(self.right_fit[0] * self.fity ** 2 + self.right_fit[1] * self.fity + self.right_fit[2])
+        self._set_vars(leftx, rightx, lefty, righty, self.left_fit, self.right_fit, self.fit_leftx, self.fit_rightx)
 
-    def _set_variables(self, leftx, rightx, lefty, righty, left_fit, right_fit, fit_leftx, fit_rightx):
+    def _set_vars(self, leftx, rightx, lefty, righty, left_fit, right_fit, fit_leftx, fit_rightx):
         self.leftx, self.lefty = leftx, lefty
         self.rightx, self.righty = rightx, righty
         self.left_fit, self.right_fit = left_fit, right_fit
         self.fit_leftx, self.fit_rightx = fit_leftx, fit_rightx
 
-    def calculate_curvature(self, real=False):
-        # Around middle of the image, I think it will give use better
-        # results since we can get better value from perspective
-        # transform
-        y_eval = 360
-        ploty = np.linspace(0, 719, num=720)
-        quadratic_coeff = 3e-4  # arbitrary quadratic coefficient
+    def evaluate_curvature_radius(self, ):
+        y_eval = 720
+        ym_per_pix = 30 / 720
+        xm_per_pix = 3.7 / 700
 
-        if real:
-            ym_per_pix = 30 / 720
-            xm_per_pix = 3.7 / 700
+        left_fit_cr = np.polyfit(self.lefty * ym_per_pix, self.leftx * xm_per_pix, 2)
+        right_fit_cr = np.polyfit(self.righty * ym_per_pix, self.rightx * xm_per_pix, 2)
 
-            left_fit_cr = np.polyfit(self.lefty * ym_per_pix,
-                                     self.leftx * xm_per_pix, 2)
-            right_fit_cr = np.polyfit(self.righty * ym_per_pix,
-                                      self.rightx * xm_per_pix, 2)
-
-        else:
-            left_fit_cr = self.left_fit
-            right_fit_cr = self.right_fit
-            # right_curverad = self.right_fit
-            ym_per_pix = 1.0
-            xm_per_pix = 1.0
-
-        left_curverad = ((1 + (2 * left_fit_cr[0] * y_eval * ym_per_pix +
-                               left_fit_cr[1]) ** 2) ** 1.5) / np.absolute(2 * left_fit_cr[0])
-        right_curverad = ((1 + (2 * right_fit_cr[0] * y_eval * ym_per_pix +
-                                right_fit_cr[1]) ** 2) ** 1.5) / np.absolute(2 * right_fit_cr[0])
-
+        left_curverad = ((1 + (2 * left_fit_cr[0] * y_eval * ym_per_pix + left_fit_cr[1]) ** 2) ** 1.5) / np.absolute(2 * left_fit_cr[0])
+        right_curverad = ((1 + (2 * right_fit_cr[0] * y_eval * ym_per_pix + right_fit_cr[1]) ** 2) ** 1.5) / np.absolute(2 * right_fit_cr[0])
         return left_curverad, right_curverad
 
-    def translate_to_real_world_image(self, image, Minv):
-        warped = self.binary_warped
-        warp_zero = np.zeros_like(warped).astype(np.uint8)
-        color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
-
-        pts_left = np.array([np.transpose(
-            np.vstack([self.fit_leftx, self.fity]))])
-        pts_right = np.array([np.flipud(np.transpose(
-            np.vstack([self.fit_rightx, self.fity])))])
-
-        pts = np.hstack((pts_left, pts_right))
-        cv2.fillPoly(color_warp, np.int_([pts]), (0, 255, 0))
-
-        newwarp = cv2.warpPerspective(color_warp, Minv, self.img_shape)
-        # image = cv2.warpPerspective(image, Minv, self.img_shape)
-        left_curvature, right_curvature = self.calculate_curvature(real=True)
-
-        cv2.putText(image, "Curvature: " + str(int(left_curvature)) + ' (m)',
-                    (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), thickness=3)
-        cv2.putText(image, str("%.2f" % self.calculate_relative_position()),
-                    (100, 170), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), thickness=3)
-
-        result = cv2.addWeighted(image, 1, newwarp, 0.3, 0)
-        return result
-
-    def calculate_relative_position(self, xm_per_pix=3.7 / 700):
+    def car_center_offset(self, xm_per_pix=3.7 / 700):
         middle = 360
-        left = self.calculate_val(self.left_fit, middle)
-        right = self.calculate_val(self.right_fit, middle)
+        left = self._calculate_val(self.left_fit, middle)
+        right = self._calculate_val(self.right_fit, middle)
         car_middle_pixel = int((left + right) / 2)
         screen_off_center = middle - car_middle_pixel
         return xm_per_pix * screen_off_center
 
-    def calculate_val(self, fit, val):
+    def _calculate_val(self, fit, val):
         return fit[0] * val ** 2 + fit[1] * val + fit[2]
